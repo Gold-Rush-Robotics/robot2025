b00a4d385b3f01aeeb2e100b8724278d7a6b4ca3
Noah <npbriggs15@gmail.com>
2025-03-29 03:15:19 -0400

    Create sorter.ino
    added sorter initial test code

---
+++ ino/sorter.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo myServo1;
+
+
+int pos = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  myServo1.attach(9);
+  myServo1.write(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (pos = 0; pos < 156; pos+= 1) {
+    myServo1.write(pos);
+    delay(50);
+  } 
+  for (pos = 156; pos > 0; pos -= 1) {
+    myServo1.write(pos);
+    delay(50);
+  }
+
+}
